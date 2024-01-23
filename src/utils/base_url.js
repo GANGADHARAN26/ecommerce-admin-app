114830d700ad74b8dc0c3ef9783dfac18915b3a6
GANGADHARAN26 <gangadharana01@gmail.com>
2024-01-23 08:25:57 +0530

    base_url

--- src/utils/base_url.js
+++ src/utils/base_url.js
@@ -1,2 +1,2 @@
-// export const base_url='https://ecommerce-backend-yp85.onrender.com/api/';
-export const base_url='http://localhost:5050/api/';
+export const base_url='https://ecommerce-backend-yp85.onrender.com/api/';
+// export const base_url='http://localhost:5050/api/';
